d379e535a71c1c9462599ba3af8e0611196dd5b6
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-01 12:15:01 +0100

    include detail component inside app template

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CONTACTS} from '../fixtures/contacts';
 
 @Component({
   selector: 'app-contact-detail',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-detail.component.scss']
 })
 export class ContactDetailComponent {
-
+  contact = CONTACTS[0];
 }
