c6609eb0d040c446bf790c4d22c107328e10956f
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-01 12:08:40 +0100

    ng generate component ContactDetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule
