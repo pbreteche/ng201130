2986bf5b5516c373f453f74264b330a4172b139c
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-01 10:27:05 +0100

    Display first contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CONTACTS } from './fixtures/contacts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng201130';
+  contact = CONTACTS[0];
 }
---
+++ src/app/fixtures/contacts.ts
@@ -0,0 +1,8 @@
+export const CONTACTS = [
+  { username: 'Pierre', email: 'pierre@example.com' },
+  { username: 'Alexis', email: 'alexis@example.com' },
+  { username: 'Daniel', email: 'daniel@example.com' },
+  { username: 'Endah', email: 'endah@example.com' },
+  { username: 'Séverine', email: 'severine@example.com' },
+  { username: 'Valentin', email: 'valentin@example.com' },
+];
