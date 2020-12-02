cc7d82af5cf8cc219b43dacbdd4b287e844e32d7
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-02 14:48:23 +0100

    Delete useless component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 import { ContactNavComponent } from './contact-nav/contact-nav.component';
-import { ContactCreateComponent } from './contact-create/contact-create.component';
 import { ContactCreateReactiveComponent } from './contact-create-reactive/contact-create-reactive.component';
-import {RoutingModule} from './routing/routing.module';
+import { RoutingModule } from './routing/routing.module';
 import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { ContactEditComponent } from './contact-edit/contact-edit.component';
     AppComponent,
     ContactDetailComponent,
     ContactNavComponent,
-    ContactCreateComponent,
     ContactCreateReactiveComponent,
     ContactEditComponent
   ],
