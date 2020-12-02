63a23fe1f43a451fc6a2ba6c0689079f205b7073
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-02 11:29:03 +0100

    Get contact detail from route param

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Contact, CONTACTS} from '../fixtures/contacts';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.scss']
 })
-export class ContactDetailComponent {
+export class ContactDetailComponent implements OnInit {
   contact: Contact = CONTACTS[0];
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(map => {
+      this.contact = CONTACTS[+map.get('id') - 1];
+    });
+  }
 }
