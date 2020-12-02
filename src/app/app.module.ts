6678c8727c7624a7ff42bb7d4b91d4697a894e4c
Pierre Bretéché <pbreteche@dawan.fr>
2020-12-02 10:03:25 +0100

    First static routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RoutingModule } from './routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 import { ContactNavComponent } from './contact-nav/contact-nav.component';
 import { ContactCreateComponent } from './contact-create/contact-create.component';
 import { ContactCreateReactiveComponent } from './contact-create-reactive/contact-create-reactive.component';
+import {RoutingModule} from './routing/routing.module';
 
 @NgModule({
   declarations: [
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactNavComponent } from '../contact-nav/contact-nav.component';
+import { ContactCreateReactiveComponent } from '../contact-create-reactive/contact-create-reactive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ContactNavComponent },
+  { path: 'nouveau', component: ContactCreateReactiveComponent },
+];
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class RoutingModule { }
